ade95b70a7bf4333946c7ac46390329e05edb197
khabzox <152230716+khabzox@users.noreply.github.com>
2025-01-26 16:21:15 +0100

    add Clerk middleware for authentication and update dependencies

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import urlRoutes from "./routes/urlRoutes.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import urlRoutes from "./routes/urlRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
+
+/*
+  Use Clerk middleware to handle authentication and authorization for Clerk.com
+  visit https://clerk.com/docs/quickstarts/express for more information
+*/
+app.use(clerkMiddleware());
 
 // Connect to MongoDB
-connectDB()
+connectDB();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.use("/api", urlRoutes)
+app.use("/api", urlRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
