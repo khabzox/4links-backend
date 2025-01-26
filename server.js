ab621f357c90883580a7f3708b8da4455d46960c
khabzox <152230716+khabzox@users.noreply.github.com>
2025-01-26 17:27:42 +0100

    refactor: restructure database connection and middleware setup, remove Redis caching

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import connectDB from "./lib/db.js";
 import urlRoutes from "./routes/urlRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 import cluster from "cluster";
 import os from "os";
-import redis from "redis";
 
 dotenv.config();
 
 const app = express();
-
-// Redis client setup
-const client = redis.createClient();
-client.on("error", (err) => {
-  console.error("Redis error:", err);
-});
 
 /*
   Use Clerk middleware to handle authentication and authorization for Clerk.com
@@ -30,21 +23,6 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Cache middleware
-const cache = (req, res, next) => {
-  const { id } = req.params;
-
-  client.get(id, (err, data) => {
-    if (err) throw err;
-
-    if (data !== null) {
-      res.send(JSON.parse(data)); // Serve cached data
-    } else {
-      next(); // Proceed to the next middleware
-    }
-  });
-};
 
 // Check if the current process is the master process
 if (cluster.isMaster) {
@@ -64,17 +42,6 @@ if (cluster.isMaster) {
 } else {
   // Worker processes
   const PORT = process.env.PORT || 5000;
-
-  // Apply cache middleware to specific routes
-  app.use("/api/links/:id", cache, (req, res) => {
-    // Simulate fetching data from a database
-    const data = { id: req.params.id, value: "Some data" };
-
-    // Save data to Redis
-    client.setex(req.params.id, 3600, JSON.stringify(data));
-
-    res.json(data);
-  });
 
   app.use("/api", urlRoutes);
 
